b3e7fc5f2af56548aca7612e47192317954a7726
nik <nnaiker01@gmail.com>
2023-09-24 22:18:06 +1200

    removed anon function

--- src/app/components/EventCards.jsx
+++ src/app/components/EventCards.jsx
@@ -1,39 +1,39 @@
 import Image from "next/image";
 
-export default function () {
-  const eventDetails = [
-    {
-      title: "Date",
-      value: "November 18th, 2023",
-      imageSrc: "/svg/calendar.svg",
-    },
-    {
-      title: "Time",
-      value: "7:00 PM - 11:00 AM",
-      imageSrc: "/svg/clock.svg",
-    },
-    {
-      title: "Age Limit",
-      value: "Only guests over 18 will be allowed in the venue.",
-      imageSrc: "/svg/child.svg",
-    },
-    {
-      title: "Guests",
-      value:
-        "MGM students and alumni, outsiders welcome with a companion ticket.",
-      imageSrc: "/svg/person.svg",
-    },
-    {
-      title: "Security",
-      value: "ID checks will be conducted at the venue entrance.",
-      imageSrc: "/svg/security.svg",
-    },
-    {
-      title: "Venue",
-      value: "Albert Park Hall, Suva",
-      imageSrc: "/svg/location.svg",
-    },
-  ];
+const eventDetails = [
+  {
+    title: "Date",
+    value: "November 18th, 2023",
+    imageSrc: "/svg/calendar.svg",
+  },
+  {
+    title: "Time",
+    value: "7:00 PM - 11:00 AM",
+    imageSrc: "/svg/clock.svg",
+  },
+  {
+    title: "Age Limit",
+    value: "Only guests over 18 will be allowed in the venue.",
+    imageSrc: "/svg/child.svg",
+  },
+  {
+    title: "Guests",
+    value:
+      "MGM students and alumni, outsiders welcome with a companion ticket.",
+    imageSrc: "/svg/person.svg",
+  },
+  {
+    title: "Security",
+    value: "ID checks will be conducted at the venue entrance.",
+    imageSrc: "/svg/security.svg",
+  },
+  {
+    title: "Venue",
+    value: "Albert Park Hall, Suva",
+    imageSrc: "/svg/location.svg",
+  },
+];
+export default function EventCards() {
   return (
     <div className="flex flex-row flex-wrap gap-6 justify-center max-w-5xl mx-auto items-center mt-8 md:mt-12 ">
       {eventDetails.map((el, key) => {
