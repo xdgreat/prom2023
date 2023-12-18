5baa81a29969a709660b79cc5f5e1a99506c20ac
xdgreat <nnaiker01@gmail.com>
2023-12-19 02:44:31 +1200

    testing google ads

--- src/app/layout.js
+++ src/app/layout.js
@@ -40,6 +40,21 @@ export default function RootLayout({ children }) {
         <NavBar />
         <Analytics />
         <div id="blur">{children}</div>
+        <script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9331570343272182"
+          crossorigin="anonymous"
+        ></script>
+        {/* <!-- test --> */}
+        <ins
+          class="adsbygoogle"
+          style="display:block"
+          data-ad-client="ca-pub-9331570343272182"
+          data-ad-slot="9208354729"
+          data-ad-format="auto"
+          data-full-width-responsive="true"
+        ></ins>
+        <script>(adsbygoogle = window.adsbygoogle || []).push({});</script>
       </body>
     </html>
   );
