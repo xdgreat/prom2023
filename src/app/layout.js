86bf9fd8f830b356cf15498b4198ffe39e48cbab
xdgreat <nnaiker01@gmail.com>
2023-12-19 02:54:13 +1200

    updated google maps

--- src/app/layout.js
+++ src/app/layout.js
@@ -16,6 +16,13 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9331570343272182"
+          crossorigin="anonymous"
+        ></script>
+      </head>
       <body className={inter.className}>
         {/* <head>
           <link
