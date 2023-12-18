cc76502aee636dde162511707cc9cc2775c8f4be
xdgreat <nnaiker01@gmail.com>
2023-12-19 02:55:28 +1200

    added meta tag

--- src/app/layout.js
+++ src/app/layout.js
@@ -17,6 +17,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
+        <meta name="google-adsense-account" content="ca-pub-9331570343272182" />
         <script
           async
           src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9331570343272182"
